b9d6e08d7b4eadb98fef1ec7b9095575240fb18c
SabinaySwain <swain.sabinay@gmail.com>
2025-10-30 19:13:33 +0530

    Greting file added

---
+++ Greeting.py
@@ -0,0 +1,5 @@
+""" personallise greeting"""
+First_Name = input("Enter your First Name: ")
+Last_Name = input("Enter your Last Name: ")
+print("Hello," + First_Name + " " + Last_Name+"!"+" "+"welcome to python program")
+print(f"Hello,{First_Name} {Last_Name}! welcome to python program")
