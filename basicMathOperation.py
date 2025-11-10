d2db610e4d4163339026892cfa39831fe6a3069b
SabinaySwain <swain.sabinay@gmail.com>
2025-11-10 19:35:29 +0530

    files added

---
+++ basicMathOperation.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number"))
+b =int(input("Enter the second number"))
+#c = int(input("Enter the third number"))
+''' BASIC MATH OPERATION'''
+Addition = a+b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+print("Addition: ",Addition)
+print("Subtraction: ",Subtraction)
+print("Multiplication: ",Multiplication)
+print("Division: ",Division)
