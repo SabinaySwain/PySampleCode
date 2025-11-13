e2eb6815a8bb5732c8a1cbdd62e7b3c8b19b3567
SabinaySwain <swain.sabinay@gmail.com>
2025-11-13 19:43:58 +0530

    factorial of a number comment added

--- Factorial_Asignment_3.py
+++ Factorial_Asignment_3.py
@@ -1,8 +1,10 @@
 def factorial(n):
+    """factorial of a number """
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n-1)
 
 x = int(input("Enter a number to calculate factorial: "))
+help(factorial)
 print(factorial(x))
